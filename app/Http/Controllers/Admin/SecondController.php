fb0d0df06e68ad6d484b2401341e4907af123a60
amr <amrmostafa0366@gmail.com>
2020-10-21 17:27:37 +0200

    =first commit

---
+++ app/Http/Controllers/Admin/SecondController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Routing\Controller;
+
+class SecondController extends Controller
+{
+    //
+
+    public function showstring(){
+
+        return 'haahhaah';
+    }
+}
